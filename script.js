af00080db9dde34e7870d90dd6a8989dc9890080
Tyler <shirtandtieler@users.noreply.github.com>
2018-06-13 14:12:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+/* jslint browser */
+/* global window,yearcal1,yearcal2 */
+
+var document = window.document;
+
+function highlightIntvl0() {
+        yearcal1.highlight([new Date()]);
+        yearcal2.highlight([new Date()]);
+}
+
+function highlightIntvl3() {
+	yearcal1.highlight([new Date(2016, 1, 29), new Date(2016, 4, 24)]);
+	yearcal2.highlight([new Date()]);
+}
+
+function highlightIntvl4() {
+	yearcal1.highlight([new Date(2016, 2, 28), new Date(2016, 2, 29), new Date(2016, 2, 31), new Date(2016, 3, 3), new Date(2016, 3, 4), new Date(2016, 3, 5), new Date(2016, 3, 10), new Date(2016, 3, 11), new Date(2016, 3, 13), new Date(2016, 3, 18), new Date(2016, 3, 21), new Date(2016, 3, 22), new Date(2016, 3, 23), new Date(2016, 3, 27), new Date(2016, 3, 28), new Date(2016, 4, 1), new Date(2016, 4, 3), new Date(2016, 4, 5), new Date(2016, 4, 6), new Date(2016, 4, 7), new Date(2016, 4, 8), new Date(2016, 4, 11), new Date(2016, 4, 14), new Date(2016, 4, 16), new Date(2016, 4, 17), new Date(2016, 4, 18), new Date(2016, 4, 19), new Date(2016, 4, 21), new Date(2016, 4, 23), new Date(2016, 4, 25), new Date(2016, 4, 29), new Date(2016, 4, 30), new Date(2016, 4, 31), new Date(2016, 5, 1), new Date(2016, 5, 2), new Date(2016, 5, 3), new Date(2016, 5, 5), new Date(2016, 5, 6), new Date(2016, 5, 7), new Date(2016, 5, 8), new Date(2016, 5, 9), new Date(2016, 5, 10), new Date(2016, 5, 15), new Date(2016, 5, 16), new Date(2016, 5, 19), new Date(2016, 5, 21), new Date(2016, 5, 23), new Date(2016, 5, 25), new Date(2016, 5, 26), new Date(2016, 5, 28), new Date(2016, 5, 29), new Date(2016, 5, 30)]);
+	yearcal2.highlight([new Date(2016, 6, 1), new Date(2016, 6, 2), new Date(2016, 6, 3), new Date(2016, 6, 4), new Date(2016, 6, 5), new Date(2016, 6, 6), new Date(2016, 6, 17), new Date(2016, 6, 21), new Date(2016, 6, 22), new Date(2016, 6, 23), new Date(2016, 6, 27), new Date(2016, 6, 29), new Date(2016, 7, 2), new Date(2016, 7, 4), new Date(2016, 7, 17), new Date(2016, 7, 18), new Date(2016, 7, 19), new Date(2016, 7, 23), new Date(2016, 7, 24), new Date(2016, 8, 19), new Date(2016, 8, 21), new Date(2016, 9, 5), new Date(2016, 9, 9), new Date(2016, 11, 25)]);
+}
+
+function highlightIntvl5() {
+	yearcal1.highlight([new Date(2016, 0, 18), new Date(2016, 2, 11), new Date(2016, 2, 14), new Date(2016, 2, 15), new Date(2016, 2, 16), new Date(2016, 2, 18), new Date(2016, 2, 24), new Date(2016, 2, 25), new Date(2016, 2, 26), new Date(2016, 2, 27), new Date(2016, 2, 30), new Date(2016, 3, 2), new Date(2016, 3, 6), new Date(2016, 3, 7), new Date(2016, 3, 8), new Date(2016, 3, 9), new Date(2016, 3, 12), new Date(2016, 3, 14), new Date(2016, 3, 15), new Date(2016, 3, 17), new Date(2016, 3, 19), new Date(2016, 3, 20), new Date(2016, 3, 24), new Date(2016, 3, 25), new Date(2016, 3, 26), new Date(2016, 3, 29), new Date(2016, 3, 30), new Date(2016, 4, 2), new Date(2016, 4, 4), new Date(2016, 4, 9), new Date(2016, 4, 10), new Date(2016, 4, 12), new Date(2016, 4, 13), new Date(2016, 4, 15), new Date(2016, 4, 20), new Date(2016, 4, 22), new Date(2016, 4, 26), new Date(2016, 4, 27), new Date(2016, 4, 28), new Date(2016, 5, 4), new Date(2016, 5, 11), new Date(2016, 5, 12), new Date(2016, 5, 13), new Date(2016, 5, 14), new Date(2016, 5, 17), new Date(2016, 5, 18), new Date(2016, 5, 20), new Date(2016, 5, 22), new Date(2016, 5, 24), new Date(2016, 5, 27)] );
+	yearcal2.highlight([new Date(2016, 6, 7), new Date(2016, 6, 8), new Date(2016, 6, 9), new Date(2016, 6, 11), new Date(2016, 6, 12), new Date(2016, 6, 13), new Date(2016, 6, 14), new Date(2016, 6, 15), new Date(2016, 6, 16), new Date(2016, 6, 19), new Date(2016, 6, 20), new Date(2016, 6, 24), new Date(2016, 6, 25), new Date(2016, 6, 26), new Date(2016, 6, 28), new Date(2016, 6, 30), new Date(2016, 6, 31), new Date(2016, 7, 1), new Date(2016, 7, 3), new Date(2016, 7, 5), new Date(2016, 7, 7), new Date(2016, 7, 8), new Date(2016, 7, 10), new Date(2016, 7, 11), new Date(2016, 7, 12), new Date(2016, 7, 14), new Date(2016, 7, 15), new Date(2016, 7, 16), new Date(2016, 7, 20), new Date(2016, 7, 21), new Date(2016, 7, 22), new Date(2016, 7, 25), new Date(2016, 7, 26), new Date(2016, 7, 28), new Date(2016, 8, 4), new Date(2016, 8, 5), new Date(2016, 8, 6), new Date(2016, 8, 7), new Date(2016, 8, 14), new Date(2016, 8, 16), new Date(2016, 8, 18), new Date(2016, 8, 20), new Date(2016, 8, 22), new Date(2016, 8, 23), new Date(2016, 8, 25), new Date(2016, 8, 26), new Date(2016, 8, 28), new Date(2016, 8, 29), new Date(2016, 9, 1), new Date(2016, 9, 3), new Date(2016, 9, 4), new Date(2016, 9, 6), new Date(2016, 9, 7), new Date(2016, 9, 8), new Date(2016, 9, 10), new Date(2016, 9, 11), new Date(2016, 9, 19), new Date(2016, 9, 20), new Date(2016, 9, 21), new Date(2016, 9, 26), new Date(2016, 9, 27), new Date(2016, 9, 28), new Date(2016, 9, 29), new Date(2016, 9, 30), new Date(2016, 9, 31), new Date(2016, 10, 1), new Date(2016, 10, 3), new Date(2016, 10, 6), new Date(2016, 10, 8), new Date(2016, 10, 9), new Date(2016, 10, 10), new Date(2016, 10, 12), new Date(2016, 10, 20), new Date(2016, 10, 21), new Date(2016, 10, 22), new Date(2016, 10, 23), new Date(2016, 10, 24), new Date(2016, 10, 25), new Date(2016, 10, 27), new Date(2016, 10, 28), new Date(2016, 10, 29), new Date(2016, 11, 9), new Date(2016, 11, 12), new Date(2016, 11, 13), new Date(2016, 11, 14), new Date(2016, 11, 16), new Date(2016, 11, 18), new Date(2016, 11, 20), new Date(2016, 11, 21), new Date(2016, 11, 22), new Date(2016, 11, 24), new Date(2016, 11, 26), new Date(2016, 11, 31)]);
+}
+
+function highlightIntvl6() {
+	yearcal1.highlight([new Date(2016, 0, 1), new Date(2016, 0, 4), new Date(2016, 0, 25), new Date(2016, 0, 26), new Date(2016, 1, 1), new Date(2016, 1, 2), new Date(2016, 1, 7), new Date(2016, 1, 11), new Date(2016, 1, 14), new Date(2016, 1, 15), new Date(2016, 1, 16), new Date(2016, 1, 19), new Date(2016, 1, 22), new Date(2016, 2, 1), new Date(2016, 2, 10), new Date(2016, 2, 12), new Date(2016, 2, 13), new Date(2016, 2, 17), new Date(2016, 2, 19), new Date(2016, 2, 21), new Date(2016, 2, 22), new Date(2016, 3, 1), new Date(2016, 3, 16)]);
+	yearcal2.highlight([new Date(2016, 6, 10), new Date(2016, 6, 18), new Date(2016, 7, 6), new Date(2016, 7, 9), new Date(2016, 7, 13), new Date(2016, 7, 27), new Date(2016, 7, 29), new Date(2016, 7, 31), new Date(2016, 8, 2), new Date(2016, 8, 3), new Date(2016, 8, 8), new Date(2016, 8, 10), new Date(2016, 8, 11), new Date(2016, 8, 12), new Date(2016, 8, 13), new Date(2016, 8, 15), new Date(2016, 8, 17), new Date(2016, 8, 24), new Date(2016, 8, 27), new Date(2016, 8, 30), new Date(2016, 9, 2), new Date(2016, 9, 12), new Date(2016, 9, 13), new Date(2016, 9, 14), new Date(2016, 9, 15), new Date(2016, 9, 17), new Date(2016, 9, 18), new Date(2016, 9, 22), new Date(2016, 9, 24), new Date(2016, 9, 25), new Date(2016, 10, 2), new Date(2016, 10, 4), new Date(2016, 10, 5), new Date(2016, 10, 7), new Date(2016, 10, 11), new Date(2016, 10, 13), new Date(2016, 10, 14), new Date(2016, 10, 15), new Date(2016, 10, 16), new Date(2016, 10, 17), new Date(2016, 10, 18), new Date(2016, 10, 26), new Date(2016, 10, 30), new Date(2016, 11, 1), new Date(2016, 11, 2), new Date(2016, 11, 3), new Date(2016, 11, 5), new Date(2016, 11, 6), new Date(2016, 11, 7), new Date(2016, 11, 8), new Date(2016, 11, 10), new Date(2016, 11, 11), new Date(2016, 11, 15), new Date(2016, 11, 17), new Date(2016, 11, 19), new Date(2016, 11, 23), new Date(2016, 11, 27), new Date(2016, 11, 28), new Date(2016, 11, 29), new Date(2016, 11, 30)]);
+}
+
+function highlightIntvl7() {
+	yearcal1.highlight([new Date(2016, 0, 2), new Date(2016, 0, 3), new Date(2016, 0, 5), new Date(2016, 0, 7), new Date(2016, 0, 8), new Date(2016, 0, 9), new Date(2016, 0, 10), new Date(2016, 0, 11), new Date(2016, 0, 12), new Date(2016, 0, 13), new Date(2016, 0, 14), new Date(2016, 0, 15), new Date(2016, 0, 16), new Date(2016, 0, 17), new Date(2016, 0, 21), new Date(2016, 0, 22), new Date(2016, 0, 23), new Date(2016, 0, 24), new Date(2016, 0, 28), new Date(2016, 0, 29), new Date(2016, 0, 30), new Date(2016, 0, 31), new Date(2016, 1, 3), new Date(2016, 1, 4), new Date(2016, 1, 5), new Date(2016, 1, 6), new Date(2016, 1, 8), new Date(2016, 1, 9), new Date(2016, 1, 10), new Date(2016, 1, 12), new Date(2016, 1, 17), new Date(2016, 1, 18), new Date(2016, 1, 20), new Date(2016, 1, 21), new Date(2016, 1, 23), new Date(2016, 1, 25), new Date(2016, 1, 26), new Date(2016, 1, 27), new Date(2016, 1, 28), new Date(2016, 2, 3), new Date(2016, 2, 4), new Date(2016, 2, 7), new Date(2016, 2, 8), new Date(2016, 2, 9), new Date(2016, 2, 20)]);
+	yearcal2.highlight([new Date(2016, 7, 30), new Date(2016, 8, 1), new Date(2016, 9, 16), new Date(2016, 9, 23), new Date(2016, 10, 19), new Date(2016, 11, 4)]);
+}
+
+function highlightIntvl8() {
+	yearcal1.highlight([new Date(2016, 0, 6), new Date(2016, 0, 19), new Date(2016, 0, 20), new Date(2016, 0, 27), new Date(2016, 1, 13), new Date(2016, 1, 24), new Date(2016, 2, 2), new Date(2016, 2, 5), new Date(2016, 2, 6)]);
+	yearcal2.highlight([new Date(2016, 8, 9)]);
+}
+
+function highlightIntvl9() {
+	yearcal1.highlight([new Date(2016, 2, 23)]);
+	yearcal2.highlight([new Date()]);
+}
+
+function checkWhich() {
+	if (document.getElementById("interval0").checked == true) {
+	highlightIntvl0();
+	} else if (document.getElementById("interval3").checked == true){
+	highlightIntvl3();
+	} else if (document.getElementById("interval4").checked == true){
+	highlightIntvl4();
+	} else if (document.getElementById("interval5").checked == true){
+	highlightIntvl5();
+	} else if (document.getElementById("interval6").checked == true){
+	highlightIntvl6();
+	} else if (document.getElementById("interval7").checked == true){
+	highlightIntvl7(); 
+	} else if (document.getElementById("interval8").checked == true){
+	highlightIntvl8();
+	} else if (document.getElementById("interval9").checked == true){
+	highlightIntvl9();
+	} else {
+		window.alert("This shouldn't ever show");
+	}
+}
